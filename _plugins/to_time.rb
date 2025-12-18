754ceb9b242f94e2b4143c35f3f451e06f45f918
aleksuuu <qwu2@berklee.edu>
2025-12-18 17:35:14 -0500

    Update to_time.rb

--- _plugins/to_time.rb
+++ _plugins/to_time.rb
@@ -1,7 +1,7 @@
 module Jekyll
     module AssetFilter
         def to_time(date_string)
-            DateTime.parse(date_string).to_time
+            Jekyll::Utils.parse_date(date_string)
         end
     end
 end
@@ -9,3 +9,4 @@ end
 Liquid::Template.register_filter(Jekyll::AssetFilter)
 
 # https://talk.jekyllrb.com/t/site-time-tomorrow/6658/14
+# changed `DateTime.parse(date_string).to_time` to `Jekyll::Utils.parse_date(date_string)` because possible Ruby version mismatches between local and GitHub
